ac48dda046c04125c58efaa0fcce9adcc8d06815
Mahmoud Farahat <Mahmoud.Farahat@corp.arab-computers.com>
2023-01-09 14:09:43 +0200

    1.2

--- dashboard/Controllers/CustomersController.cs
+++ dashboard/Controllers/CustomersController.cs
@@ -24,19 +24,58 @@ namespace dashboard.Controllers
             var pageSize = int.Parse(Request.QueryString["length"]);
 
             var skip = int.Parse(Request.QueryString["start"]);
-            var searchValue = Request.QueryString["search[value]"];
+            //var searchValue = Request.QueryString["search[value]"];
 
             var sortColumn = Request.QueryString[string.Concat("columns[", Request.QueryString["order[0][column]"],"][name]")];
             var sortColumnDirection = Request.QueryString["order[0][dir]"];
-            
-            IQueryable<Customer> customers = db.Customers.Where(m => string.IsNullOrEmpty(searchValue) ? true : (m.FirstName.Contains(searchValue)) || (m.LastName.Contains(searchValue)) ||
-                 (m.Contact.Contains(searchValue))  || (m.Email.Contains(searchValue))
-                );
+
+
+            IQueryable<Customer> customers = db.Customers;
+
+
+            //IQueryable<Customer> customers = db.Customers
+            //.Where(m => string.IsNullOrEmpty(searchValue) ? true : (m.FirstName.Contains(searchValue)) || (m.LastName.Contains(searchValue)) ||
+            //     (m.Contact.Contains(searchValue)) || (m.Email.Contains(searchValue))
+            //    );
+
+
+            if (!string.IsNullOrEmpty(Request.QueryString["columns[0][search][value]"]))
+            {
+                var firstNameSearchValue = Request.QueryString["columns[0][search][value]"].ToLower();
+                customers = customers.Where(x => x.FirstName.ToLower().Contains(firstNameSearchValue));
+            }
+               
+            if (!string.IsNullOrEmpty(Request.QueryString["columns[1][search][value]"]))
+            {
+                var lastNameSearchValue = Request.QueryString["columns[1][search][value]"].ToLower();
+                customers = customers.Where(x => x.LastName.ToLower().Contains(lastNameSearchValue));
+            }               
+            if (!string.IsNullOrEmpty(Request.QueryString["columns[2][search][value]"]))
+            {
+                var contactSearchValue = Request.QueryString["columns[2][search][value]"].ToLower();
+                customers = customers.Where(x => x.Contact.ToLower().Contains(contactSearchValue));
+
+            }
+            if (!string.IsNullOrEmpty(Request.QueryString["columns[3][search][value]"]))
+            {
+                var emailSearchValue = Request.QueryString["columns[3][search][value]"].ToLower();
+                customers = customers.Where(x => x.Email.ToLower().Contains(emailSearchValue));
+
+            }
+            if (!string.IsNullOrEmpty(Request.QueryString["columns[4][search][value]"]))
+            {
+                var DateOfBirthSearchValue = Request.QueryString["columns[4][search][value]"].ToLower();
+                customers = customers.Where(x => x.DateOfBirth.ToString().Contains(DateOfBirthSearchValue));
+
+            }
+
+
 
             if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortColumnDirection)))
             {
                 customers = customers.OrderBy(string.Concat(sortColumn, " ", sortColumnDirection));
             }
+
             //switch (sortColumn)
             //{
             //    case "Id":
@@ -58,6 +97,7 @@ namespace dashboard.Controllers
             //        customers = sortColumnDirection == "asc" ? customers.OrderBy(a => a.DateOfBirth) : customers.OrderByDescending(a => a.DateOfBirth);
             //        break;
             //}
+
             var data = customers.Skip(skip).Take(pageSize).ToList();
             var recordsTotal = customers.Count();
 
